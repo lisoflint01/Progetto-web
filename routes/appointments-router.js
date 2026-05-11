095adf1ce3887362657e159f310fa2b3d430269a
Lisandro Antonioli <lisandro.antonioli@studio.unibo.it>
2026-05-11 16:04:13 +0200

    appointments router and controller: view, create, update, delete

--- routes/appointments-router.js
+++ routes/appointments-router.js
@@ -4,6 +4,8 @@ import * as appointmentsController from '../controllers/appointments-controller.
 const router = express.Router();
 
 router.get('/api/appointments', appointmentsController.allAppointments);
+router.get('/api/appointments/create', appointmentsController.createAppointment);
+router.get('/api/appointments/update/:id', appointmentsController.updateAppointment);
 router.get('/api/appointments/delete/:id', appointmentsController.deleteAppointment);
 
 export { router as appointmentsRouter};
