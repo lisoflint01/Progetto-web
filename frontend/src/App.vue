b9bb55c432e186d9e012725161abb3a0d9eaa403
Lisandro Antonioli <lisandro.antonioli@studio.unibo.it>
2026-05-14 17:35:04 +0200

    vue e vite

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  
+  <nav>
+    <RouterLink to="/admin">Admin</RouterLink>
+    <RouterLink to="/appointments">Appointments</RouterLink>
+    <RouterLink to="/login">Login</RouterLink>
+    <RouterLink to="/patient">Patient</RouterLink>
+  </nav>
+
+  <RouterView/>
+
+</template>
---
+++ frontend/src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createRouter, createWebHistory, type Router } from 'vue-router'
+
+import Admin from './pages/admin.vue'
+import Appointments from './pages/appointments.vue'
+import Login from './pages/login.vue'
+import Patient from './pages/patient.vue'
+
+const router: Router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/admin', component: Admin},
+        { path: '/appointments', component: Appointments},
+        { path: '/login', component: Login},
+        { path: '/patient', component: Patient},
+    ]
+})
+
+createApp(App).use(router).mount('#app')
