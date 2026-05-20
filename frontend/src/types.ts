9753dc1987c9729719a42c19930a30ed7429ccd0
Lisandro Antonioli <lisandro.antonioli@studio.unibo.it>
2026-05-20 17:47:28 +0200

    ignore

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,11 +1,27 @@
-export interface Paziente {
-    
+export interface Patient {
+    cf: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    phone: string,
+    birth_date: Date,
+    medical_history: string
 }
 
-export interface Appuntamento {
-
+export interface Appointment {
+    appointment_id?: number;  
+    patient_cf: string;      
+    date: Date;
+    initial_time: string;      
+    duration: number;           
+    note?: string;              
+    state: 'prenotato' | 'cancellato' | 'completato';
 }
 
-export interface DatiLogin {
-
+export interface User {
+    user_id?: number;          
+    username: string;         
+    password: string;          
+    fullname: string;          
+    role: 'admin' | 'operatore';
 }
