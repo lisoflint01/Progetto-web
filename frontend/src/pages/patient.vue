ee5e32af8e2a564a006c13e95551061c5e3c9847
Lisandro Antonioli <lisandro.antonioli@studio.unibo.it>
2026-05-24 15:50:38 +0200

    patient script

--- frontend/src/pages/patient.vue
+++ frontend/src/pages/patient.vue
@@ -1,13 +1,52 @@
-<script setup>
+<script>
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+        name: '',
+        surname: '',
+        cf: '',
+        email: '',
+        tel: '',
+        date_birth: '',
+        anamnesis: '',
+
+        messaggioSuccesso: ''
+        }
+    },
+
+    methods: {
+        creaPaziente() {
+            this.messaggioSuccesso= '';
+
+            axios.post('/api/patient/create', {
+                name: this.name,
+                surname: this.surname,
+                cf: this.cf,
+                email: this.email,
+                tel: this.tel,
+                date_birth: this.date_birth,
+                anamnesis: this.anamnesis
+            })
+
+            .then(response => {
+                this.messaggioSuccesso = "Paziente registrato con successo!";
+            })
+            .catch(error => {
+                this.messaggioSuccesso = "Errore durante il salvataggio.";
+                console.error(error);
+            });
+        }
+    }
+}
 </script>
-
 
 <template>
     <div>
         <h1>Registra Paziente</h1>
 
-        <form method="POST" action="/api/patient/create">
+        <form @submit.prevent="creaPaziente">
             <div class="row">
                 <div class="col-1"></div>
                 <div class="col-5">
@@ -15,21 +54,21 @@
                     <div class="mb-2 align-items-center">
                         <label for="name">Nome</label>
                         <div>
-                            <input type="text" id="name" name="name" class="input" required>
+                            <input type="text" id="name" v-model="name" class="input" required>
                         </div>
                     </div>
                     
                     <div class="mb-2 align-items-center">
                         <label for="surname">Cognome</label>
                         <div>
-                            <input type="text" id="surname" name="surname" class="input" required>
+                            <input type="text" id="surname" v-model="surname" class="input" required>
                         </div>
                     </div>
 
                     <div class="mb-2 align-items-center">
                         <label for="cf">Codice Fiscale</label>
                         <div>
-                            <input type="text" id="cf" name="cf" class="input" required>
+                            <input type="text" id="cf" v-model="cf" class="input" required>
                         </div>
                     </div>
                 </div>
@@ -38,21 +77,21 @@
                     <div class="mb-2 align-items-center">
                         <label for="email" class="col-sm-4 ">Email</label>
                         <div>
-                            <input type="email" id="email" name="email" class="input">
+                            <input type="email" id="email" v-model="email" class="input">
                         </div>
                     </div>
 
                     <div class="mb-2 align-items-center">
                         <label for="tel" class="col-sm-4 ">Telefono</label>
                         <div class="">
-                            <input type="tel" id="tel" name="tel" class="input" required>
+                            <input type="tel" id="tel" v-model="tel" class="input" required>
                         </div>
                     </div>
                     
                     <div class="mb-2 align-items-center">
                         <label for="date_birth" class="">Data Nascita</label>
                         <div>
-                            <input type="date" id="date_birth" name="date_birth" class="input" required>
+                            <input type="date" id="date_birth" v-model="date_birth" class="input" required>
                         </div>
                     </div>
                 </div>
@@ -61,12 +100,13 @@
 
             <div class="mt-3 text-center">
                 <label for="anamnesis" class="d-block mb-3">Anamnesi</label>
-                <textarea name="anamnesis" id="anamnesis" class="textarea_patient"></textarea>
+                <textarea v-model="anamnesis" id="anamnesis" class="textarea_patient"></textarea>
             </div> 
 
-            <div class="text-center mt-5">
+            <div class="text-center mt-4">
                 <button type="submit">Conferma</button>
             </div>
+            <div v-if="messaggioSuccesso">{{ messaggioSuccesso }}</div>
         </form>
     </div>
 </template>
